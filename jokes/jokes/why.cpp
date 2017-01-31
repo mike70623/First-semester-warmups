2b13d2497454f32e471a4f722aebb12e484058be
game110pm <game110pm@605-110-STU07>
2017-01-31 12:38:25 -0700

    bad pun

---
+++ jokes/jokes/why.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "My fish friend slammed into a concrete wall and hes like" << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl;
+	cout << "dam" << endl;
+}
